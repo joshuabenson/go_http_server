83f27d2fc6a8b95e819205501a10e9ffc0dfc54c
Joshua Benson <bensonjosha@gmail.com>
2021-11-28 17:38:53 -0600

    adds serving static html page

--- readme.md
+++ readme.md
@@ -1,2 +1,6 @@
-'''Go HTTP Server'''
+#Go HTTP Server
 Created using this tutorial: https://tutorialedge.net/golang/creating-simple-web-server-with-golang/
+
+###To run server
+<code>go run server.go</code>
+requests can be made at http://localhost:8081/world
--- server.go
+++ server.go
@@ -2,16 +2,32 @@ package main
 
 import (
 	"fmt"
-	"html"
 	"log"
 	"net/http"
+	"strconv"
+	"sync"
 )
 
-func main() {
+var counter int
+var mutex = &sync.Mutex{}
 
+func echoString(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hello")
+}
+
+func incrementCounter(w http.ResponseWriter, r *http.Request) {
+	mutex.Lock()
+	counter++
+	fmt.Fprintf(w, strconv.Itoa(counter))
+	mutex.Unlock()
+}
+
+func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+		http.ServeFile(w, r, r.URL.Path[1:])
 	})
+
+	http.HandleFunc("/increment", incrementCounter)
 
 	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hi")
